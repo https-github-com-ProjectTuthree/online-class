efdc9712ff9019c5e4f4b18aae2a93bdd4439508
kmj <rlaalswh531@gmail.com>
2021-10-17 07:22:02 +0900

    비디오, 마이크

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,129 @@
-alert('hi!');
+const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
+
+let myStream;
+let muted = true;
+let cameraOff = false;
+let roomName;
+let myPeerConnection;
+
+async function getCameras(){
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter((device => device.kind === "videoinput"));
+        const currentCamera = myStream.getVideoTracks()[0];
+        cameras.forEach(camera => {
+            const option = document.createElement("option")
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            if(currentCamera.label === camera.label){
+                option.selected = true;
+            }
+            camerasSelect.appendChild(option);
+        })
+        console.log(cameras);
+    } catch (e){
+        console.log(e);
+    }
+}
+
+async function getMedia(deviceId){
+    const initialConstraints = {
+        audio: true,
+        video: { facingMode: "user" },
+    };
+    const cameraConstraints = {
+        audio: true,
+        video: { deviceId: { exact: deviceId } },
+    };
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints : initialConstraints
+        );
+        myFace.srcObject = myStream;
+        if(!deviceId){
+            await getCameras();
+        }
+    }catch (e){
+        console.log(e);
+    }
+
+}
+
+getMedia();
+
+function handleMuteClick(){
+    myStream.getAudioTracks().forEach(track => (track.enabled = !track.enabled));
+    if(!muted){
+        muteBtn.innerText = "음소거 해제"
+        muted = true;
+    } else {
+        muteBtn.innerText = "음소거";
+        muted = false;
+    }
+}
+
+function handleCameraClick() {
+    myStream.getVideoTracks().forEach(track => (track.enabled = !track.enabled));
+
+    if(cameraOff){
+        cameraBtn.innerText= "카메라 끄기";
+        cameraOff = false;
+    } else {
+        cameraBtn.innerText = "카메라 켜기";
+        cameraOff = true;
+    }
+}
+
+async function handleCameraChange() {
+    await getMedia(camerasSelect.value);
+
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
+
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function startMedia(){
+    Welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+}
+
+function handleWelcomeSubmit(event){
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input");
+    socket.emit("join_room", input.value, startMedia);
+    roomName = input.value;
+    input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+//Socket Code
+
+socket.on("welcome", async() => {
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    console.log("sent the offer");
+    socket.emit("offer", offer, roomName);
+});
+
+socket.on("offer", offer => {
+    console.log(offer);
+});
+
+//RTC Code
+
+function makeConnection(){
+    mypeerConnection = new RTCPeerConnection();
+    myStream.getTracks().forEach(track => myPeerConnection.addTrack(trackm, myStream));
+}
--- src/server.js
+++ src/server.js
@@ -1,11 +1,28 @@
 import express from "express";
+import http from "http";
+import SocketIO from "socket.io";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.listen(3000);
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+    socket.on("join_room", (roomName, done) => {
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome");
+    });
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+    });
+});
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
